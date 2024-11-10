33f627c1045e2186ea0a8483ed9502bcac667b55
Ben Hunter <code@benhunter.me>
2024-11-09 21:37:44 -0600

    Demo bi-directional IPC with ipc-channel crate.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rust IPC
+
+- Demo bi-directional IPC channels based on this [example](https://github.com/servo/ipc-channel/issues/306).
+- [ipc-channel](https://crates.io/crates/ipc-channel) on crates.io
+- [ipc-channel](https://github.com/servo/ipc-channel) on GitHub
---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use ipc_channel::ipc::{self, *};
+use std::env;
+use std::process::{self, Command};
+
+pub fn spawn_server(_ignored: &str, arg: &str) -> process::Child {
+    Command::new(env::current_exe().unwrap())
+        .arg(arg)
+        .spawn()
+        .expect("failed to execute server process")
+}
+
+type Data = (String, String);
+type Bootstrap = (IpcSender<Data>, IpcReceiver<Data>);
+
+fn main() {
+    if env::args().len() == 1 {
+        println!("Inside process_handler");
+        let (server0, server_name0) = IpcOneShotServer::<Bootstrap>::new().unwrap();
+        println!("server_name0 in process_handler: {:?}", server_name0);
+        let guiserver = spawn_server(
+            "/Users/peterweyand/Code/rustprojects/project1_2/src/rungui.sh",
+            &server_name0
+        );
+        let (_receiver, (sender, receiver)): (IpcReceiver<Bootstrap>, Bootstrap) = server0.accept().unwrap();
+        let data = ("Peter".to_string(), "36".to_string());
+        sender.send(data);
+        let received = receiver.recv().unwrap();
+        println!("parent received {:?}", received);
+
+        loop {
+            let received = receiver.recv().unwrap();
+            println!("parent received len={:?}", received.1.len());
+            std::thread::sleep_ms(10);
+        }
+
+    } else {
+
+        println!("Inside GUI process");
+        let args: Vec<String> = env::args().collect();
+        let (to_child, from_parent): (IpcSender<Data>, IpcReceiver<Data>) = ipc::channel().unwrap();
+        let (to_parent, from_child): (IpcSender<Data>, IpcReceiver<Data>) = ipc::channel().unwrap();
+        let bootstrap = IpcSender::connect(args[1].clone()).unwrap();
+        bootstrap.send((to_child, from_child)).unwrap();
+        let data = from_parent.recv().unwrap();
+        println!("child received {:?}", data);
+        to_parent.send(("Dagne".to_string(), "8".to_string()));
+
+        let mut count = 1;
+        loop {
+            let val = std::iter::repeat("X").take(count).collect::<String>();
+            let data = ("send count=".to_string(), val);
+            println!("child sent. count={:?}", count);
+            to_parent.send(data);
+            std::thread::sleep_ms(1);
+            count *= 2;
+        }
+
+    }
+}
