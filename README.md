7bd4737d220f400a7a2de62139a00c185e8ecd00
Ben Hunter <code@benhunter.me>
2024-11-10 11:03:48 -0600

    Add output for parent send. Update strings. Bug: does not work on Windows.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 - Demo bi-directional IPC channels based on this [example](https://github.com/servo/ipc-channel/issues/306).
 - [ipc-channel](https://crates.io/crates/ipc-channel) on crates.io
 - [ipc-channel](https://github.com/servo/ipc-channel) on GitHub
+
+Demo does not run on Windows.
--- src/main.rs
+++ src/main.rs
@@ -18,24 +18,25 @@ fn main() {
         let (server0, server_name0) = IpcOneShotServer::<Bootstrap>::new().unwrap();
         println!("server_name0 in process_handler: {:?}", server_name0);
         let guiserver = spawn_server(
-            "/Users/peterweyand/Code/rustprojects/project1_2/src/rungui.sh",
+            "path/to/script.sh",
             &server_name0
         );
         let (_receiver, (sender, receiver)): (IpcReceiver<Bootstrap>, Bootstrap) = server0.accept().unwrap();
-        let data = ("Peter".to_string(), "36".to_string());
-        sender.send(data);
+        let data = ("KeyFromParent".to_string(), "36".to_string());
+        sender.send(data.clone()).unwrap();
+        println!("parent sent {:?}", data);
         let received = receiver.recv().unwrap();
         println!("parent received {:?}", received);
 
         loop {
             let received = receiver.recv().unwrap();
             println!("parent received len={:?}", received.1.len());
-            std::thread::sleep_ms(10);
+            std::thread::sleep(std::time::Duration::from_millis(10));
         }
 
     } else {
 
-        println!("Inside GUI process");
+        println!("Inside spawned process");
         let args: Vec<String> = env::args().collect();
         let (to_child, from_parent): (IpcSender<Data>, IpcReceiver<Data>) = ipc::channel().unwrap();
         let (to_parent, from_child): (IpcSender<Data>, IpcReceiver<Data>) = ipc::channel().unwrap();
@@ -43,7 +44,7 @@ fn main() {
         bootstrap.send((to_child, from_child)).unwrap();
         let data = from_parent.recv().unwrap();
         println!("child received {:?}", data);
-        to_parent.send(("Dagne".to_string(), "8".to_string()));
+        to_parent.send(("KeyFromChild".to_string(), "8".to_string()));
 
         let mut count = 1;
         loop {
@@ -51,7 +52,7 @@ fn main() {
             let data = ("send count=".to_string(), val);
             println!("child sent. count={:?}", count);
             to_parent.send(data);
-            std::thread::sleep_ms(1);
+            std::thread::sleep(std::time::Duration::from_millis(1));
             count *= 2;
         }
 
